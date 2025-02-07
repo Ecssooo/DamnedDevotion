0174897a4993f369decf582ff8dc3e4cc5e9c28d
Ecssooo <alexandre.lejeune9@gmail.com>
2025-02-07 11:15:14 +0100

    [ADD Level maker Tool V2

--- Assets/Scripts/Editor/LevelMaker.cs
+++ Assets/Scripts/Editor/LevelMaker.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEditor;
 
@@ -14,6 +16,31 @@ public class LevelMaker : EditorWindow
 
     private int levelToLoad = 0;
 
+    private GameObject humanPrefab;
+    private GameObject knightSwordPrefab;
+    private GameObject knightShieldPrefab;
+    private GameObject monsterPrefab;
+    private GameObject miniMonsterPrefab;
+    private GameObject cauldronPrefab;
+
+    public enum OPTIONS
+    {
+        NONE,
+        HUMAN,
+        KNIGHTSWORD,
+        KNIGHTSHIELD,
+        MONSTER,
+        MINIMONSTER,
+        CAULDRON,
+    }
+    
+    public List<OPTIONS> ops = new(12){ 
+        OPTIONS.NONE,OPTIONS.NONE, OPTIONS.NONE,
+        OPTIONS.NONE,OPTIONS.NONE,OPTIONS.NONE,
+        OPTIONS.NONE,OPTIONS.NONE,OPTIONS.NONE,
+        OPTIONS.NONE,OPTIONS.NONE,OPTIONS.NONE,
+    };
+    
     private List<GameObject> slots = new List<GameObject>() { 
         null, null, null,
         null,null,null,
@@ -28,7 +55,8 @@ public class LevelMaker : EditorWindow
         new Vector2Int(2,0), new Vector2Int(2,1), new Vector2Int(2,2),
         new Vector2Int(3,0), new Vector2Int(3,1), new Vector2Int(3,2),
     };
-    
+
+    private Vector2 scrollPos;
     
     [MenuItem("Tools/LevelMaker")]
     public static void ShowWindow()
@@ -38,6 +66,7 @@ public class LevelMaker : EditorWindow
 
     private void OnGUI()
     {
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUILayout.MaxHeight(position.height));
         GUILayout.Label("LevelMaker", EditorStyles.boldLabel);
 
         //Setup
@@ -51,11 +80,44 @@ public class LevelMaker : EditorWindow
 
         EditorGUILayout.Space(20);
         //Slots for Card
-        for (int i = 0; i < slots.Count; i++)
+        // for (int i = 0; i < slots.Count; i++)
+        // {
+        //     slots[i] = EditorGUILayout.ObjectField($"Slot {i + 1}", slots[i], typeof(GameObject), false) as GameObject;
+        // }
+        
+        humanPrefab = EditorGUILayout.ObjectField("Human", humanPrefab, typeof(GameObject), false) as GameObject;
+        knightSwordPrefab = EditorGUILayout.ObjectField("Knight Sword", knightSwordPrefab, typeof(GameObject), false) as GameObject;
+        knightShieldPrefab = EditorGUILayout.ObjectField("Knight Shield",knightShieldPrefab, typeof(GameObject), false) as GameObject;
+        monsterPrefab = EditorGUILayout.ObjectField("Monster", monsterPrefab, typeof(GameObject), false) as GameObject;
+        miniMonsterPrefab = EditorGUILayout.ObjectField("Mini Monster", miniMonsterPrefab, typeof(GameObject), false) as GameObject;
+        cauldronPrefab = EditorGUILayout.ObjectField("Cauldron", cauldronPrefab, typeof(GameObject), false) as GameObject;
+        
+        EditorGUILayout.Space(20);
+        
+        EditorGUILayout.BeginHorizontal();
+        for (int i = 0; i < 3; i++)
         {
-            slots[i] = EditorGUILayout.ObjectField($"Slot {i + 1}", slots[i], typeof(GameObject), false) as GameObject;
+            ops[i] = (OPTIONS)EditorGUILayout.EnumPopup(ops[i]);
         }
-
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.BeginHorizontal();
+        for (int i = 3; i < 6; i++)
+        {
+            ops[i] = (OPTIONS)EditorGUILayout.EnumPopup(ops[i]);
+        }
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.BeginHorizontal();
+        for (int i = 6; i < 9; i++)
+        {
+            ops[i] = (OPTIONS)EditorGUILayout.EnumPopup(ops[i]);
+        }
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.BeginHorizontal();
+        for (int i = 9; i < 12; i++)
+        {
+            ops[i] = (OPTIONS)EditorGUILayout.EnumPopup(ops[i]);
+        }
+        EditorGUILayout.EndHorizontal();
         
         EditorGUILayout.Space(20);
         //Buttons
@@ -77,6 +139,8 @@ public class LevelMaker : EditorWindow
         {
             ResetBoard();
         }
+        
+        EditorGUILayout.EndScrollView();
     }
     
     /// <summary>
@@ -85,15 +149,59 @@ public class LevelMaker : EditorWindow
     void ApplyLevel()
     {
         board.InitSlotTab();
-        int index = -1;
-        foreach (var card in slots)
+        int index = 0;
+        // foreach (var card in slots)
+        // {
+        //     index++;
+        //     if (card == null) continue;
+        //     var GO = Instantiate(card, board.transform);
+        //     var cardGO = GO.GetComponent<Card>();
+        //     cardGO.PositionOnBoard = positionSlot[index];
+        //     board.SetSlots(cardGO);
+        // }
+
+        foreach (var op in ops)
         {
+            switch (op)
+            {
+                case(OPTIONS.HUMAN):
+                    var GO = Instantiate(humanPrefab, board.transform);
+                    var cardGO = GO.GetComponent<Card>();
+                    cardGO.PositionOnBoard = positionSlot[index];
+                    board.SetSlots(cardGO);
+                    break;
+                case(OPTIONS.KNIGHTSWORD):
+                    var GO1 = Instantiate(knightSwordPrefab, board.transform);
+                    var cardGO1 = GO1.GetComponent<Card>();
+                    cardGO1.PositionOnBoard = positionSlot[index];
+                    board.SetSlots(cardGO1);
+                    break;
+                case(OPTIONS.KNIGHTSHIELD):
+                    var GO3 = Instantiate(knightShieldPrefab, board.transform);
+                    var cardGO3 = GO3.GetComponent<Card>();
+                    cardGO3.PositionOnBoard = positionSlot[index];
+                    board.SetSlots(cardGO3);
+                    break;
+                case(OPTIONS.MONSTER):
+                    var GO2 = Instantiate(monsterPrefab, board.transform);
+                    var cardGO2 = GO2.GetComponent<Card>();
+                    cardGO2.PositionOnBoard = positionSlot[index];
+                    board.SetSlots(cardGO2);
+                    break;
+                case(OPTIONS.MINIMONSTER):
+                    var GO4 = Instantiate(monsterPrefab, board.transform);
+                    var cardGO4 = GO4.GetComponent<Card>();
+                    cardGO4.PositionOnBoard = positionSlot[index];
+                    board.SetSlots(cardGO4);
+                    break;
+                case(OPTIONS.CAULDRON):
+                    var GO5 = Instantiate(cauldronPrefab, board.transform);
+                    var cardGO5 = GO5.GetComponent<Card>();
+                    cardGO5.PositionOnBoard = positionSlot[index];
+                    board.SetSlots(cardGO5);
+                    break;
+            }
             index++;
-            if (card == null) continue;
-            var GO = Instantiate(card, board.transform);
-            var cardGO = GO.GetComponent<Card>();
-            cardGO.PositionOnBoard = positionSlot[index];
-            board.SetSlots(cardGO);
         }
     }
 
@@ -110,6 +218,21 @@ public class LevelMaker : EditorWindow
         };
         List<GameObject> newCardList = new();
         int index = 0;
+
+        for (int i = 0; i < ops.Count; i++)
+        {
+            switch (ops[i])
+            {
+                case(OPTIONS.HUMAN): slots[i] = humanPrefab; break;
+                case(OPTIONS.KNIGHTSWORD): slots[i] = knightSwordPrefab; break;
+                case(OPTIONS.KNIGHTSHIELD): slots[i] = knightShieldPrefab; break;
+                case(OPTIONS.MINIMONSTER): slots[i] = miniMonsterPrefab; break;
+                case(OPTIONS.MONSTER): slots[i] = monsterPrefab; break;
+                case(OPTIONS.CAULDRON): slots[i] = cauldronPrefab; break;
+                case(OPTIONS.NONE): slots[i] = null; break;
+            }
+        }
+        
         foreach (var card in slots)
         {
             if (card != null) allPosition[index] = positionSlot[index];
@@ -128,7 +251,14 @@ public class LevelMaker : EditorWindow
     void LoadLevel()
     {
         ResetBoard();
-        board.SetLevel(_levelDatabase.levelList[levelToLoad]);
+        try
+        {
+            board.SetLevel(_levelDatabase.levelList[levelToLoad]);
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Debug.LogWarning("Level doesn't exist");
+        }
     }
 
     /// <summary>
