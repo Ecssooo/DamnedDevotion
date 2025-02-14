eac4c0d5f21e49d932c876ec9ea464e0ceaaf5cf
Theoferrarini <theoferrarini6@gmail.com>
2025-02-14 11:27:56 +0100

    [modify] InvocationPower

--- Assets/Scripts/Effects/InvocationPower.cs
+++ Assets/Scripts/Effects/InvocationPower.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class InvocationPower : MonoBehaviour
 {
+    public MonoBehaviour scriptToActivate;
 
+    void Start()
+    {
+        if (scriptToActivate != null)
+        {
+            scriptToActivate.enabled = false;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (scriptToActivate != null)
+        {
+            scriptToActivate.enabled = !scriptToActivate.enabled;
+        }
+    }
 }
