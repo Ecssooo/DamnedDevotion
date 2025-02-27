f82753564da1be5acdb6c9bc8e360e2459d50297
Kenshines <quentinchaumard@hotmail.fr>
2025-02-27 14:44:08 +0100

    [add] Audiomanager pas fini

--- Assets/Scripts/GameStateMachine/GameSetupState.cs
+++ Assets/Scripts/GameStateMachine/GameSetupState.cs
@@ -6,6 +6,8 @@ public class GameSetupState : GameBaseState
     {
         GameManager.Instance.ActionCount.InitActionPoint(GameManager.Instance.LevelDatabase.levelList[LevelManager.Instance.CurrentLevel].maxActionCount);
         GameManager.Instance.ActionCount.DisplayActionPoint();
+        AudioManager.Instance.musicSource.Stop();
+        AudioManager.Instance.PlayMusic("Level");
     }
 
     public override void UpdateState(GameStateManager manager)
@@ -19,6 +21,6 @@ public class GameSetupState : GameBaseState
 
     public override void ExitState(GameStateManager manager)
     {
-        //
+       AudioManager.Instance.musicSource.Stop();
     }
 }
