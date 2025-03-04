6170de43efe8882ca27ffaa30d42366cce0b9052
Ecssooo <alexandre.lejeune9@gmail.com>
2025-03-04 22:05:37 +0100

    [ADD] Tuto level 1

--- Assets/Scripts/Tutos/TutoLevel1.cs
+++ Assets/Scripts/Tutos/TutoLevel1.cs
@@ -1,27 +1,76 @@
+using NUnit.Framework;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class TutoLevel1 : TutoLevelBase
 {
-    private int action = 0;
+    private int action = -1;
 
     [SerializeField] private GameObject scene1;
     [SerializeField] private GameObject scene2;
+    [SerializeField] private GameObject scene3;
+    [SerializeField] private GameObject scene4;
+    [SerializeField] private GameObject scene5;
+    [SerializeField] private GameObject scene6;
     
     public override void DoTuto()
     {
-        if (action == 0 && GameManager.Instance.Effect == Effects.MOVE)
+        switch (action)
         {
-            scene1.SetActive(false);
-            scene2.SetActive(true);
-            action = 1;
-        }
-        else if (action == 1 && ListAction.Instance.ListActions.Count > 0)
-        {
-            if(ListAction.Instance.ListActions[0]._card.CardType == CardType.KNIGHTSWORD)
-            {
-                scene2.SetActive(false);
-                action = 2;
-            }
+            case(-1):
+                scene1.SetActive(true);
+                action = 0;
+                break;
+            case(0):
+                if (GameManager.Instance.Effect == Effects.MOVE)
+                {
+                    scene1.SetActive(false);
+                    scene2.SetActive(true);
+                    action = 1;
+                }
+                break;
+            case(1):
+                if (ListAction.Instance.ListActions.Count <= 0) return;
+                if(ListAction.Instance.ListActions[0]._card.CardType == CardType.KNIGHTSWORD)
+                {
+                    scene2.SetActive(false);
+                    scene3.SetActive(true);
+                    action = 2;
+                }
+                break;
+            case(2):
+                if (GameStateManager.Instance.WaitForAction)
+                {
+                    GameManager.Instance.Effect = Effects.NONE;
+                    scene3.SetActive(false);
+                    scene4.SetActive(true);
+                    action = 3;
+                }
+                break;
+            case(3):
+                if (ListAction.Instance.ListActions.Count <= 0)
+                {
+                    scene4.SetActive(false);
+                    scene5.SetActive(true);
+                    action = 4;
+                }
+                break;
+            case(4):
+                if (GameManager.Instance.Effect == Effects.MOVE)
+                {
+                    scene5.SetActive(false);
+                    scene6.SetActive(true);
+                    action = 5;
+                }
+                break;
+            case(5):
+                if (ListAction.Instance.ListActions.Count <= 0) return;
+                if (ListAction.Instance.ListActions[0]._card.CardType == CardType.KNIGHTSWORD)
+                {
+                    scene6.SetActive(false);
+                    action = 7;
+                }
+                break;
         }
     }
 }
